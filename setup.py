c141282a828fb434d3d2c01383d25e08041344eb
Askat Kuzdeuov <askhatkuzdeuov@gmail.com>
2020-03-25 12:46:36 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 12:30:58 2020
+
+@author: askat
+"""
+
+
+from setuptools import setup
+setup(
+    name='covid19_simulator',
+    version='0.0.1',
+    entry_points={
+        'console_scripts': [
+            'covid19_simulator=covid19_simulator:main'
+        ]
+    }
+)
